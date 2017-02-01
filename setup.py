f6df00cb50b3651acf93c8fb8f9a77b118f3e413
Marc Cluet <marc@ukoncherry.com>
2017-02-01 12:21:39 +0000

    Follow pypi package specs

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 """ Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. """
 from setuptools import setup
 
-install_requires = ['requests', 'simplejson']
-
 setup(name='environment_manager',
-      version='0.0.1',
+      version='0.0.2',
       description="A Client library for Environment Manager",
-      maintainer="Marc Cluet",
-      maintainer_email="marc.cluet@thetrainline.com",
-      install_requires=install_requires,
+      author="Marc Cluet",
+      author_email="marc.cluet@thetrainline.com",
+      install_requires=['requests', 'simplejson'],
       license='Apache 2.0',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache 2.0',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5'],
+      keywords='environment_manager client library development',
       package_data={'': ['LICENSE.txt']},
       packages=['environment_manager'],
       zip_safe=True)
