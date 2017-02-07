9c6341b1cfb15c8742c174e45545813941cbbbe9
Marc Cluet <marc@ukoncherry.com>
2017-02-07 12:01:54 +0000

    Push to version 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='environment_manager',
-      version='0.0.6',
+      version='0.0.7',
       description="A Client library for Environment Manager",
       url="https://github.com/trainline/python-environment_manager",
       author="Marc Cluet",
