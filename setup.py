99a28f3357fe4c90bcd922e96b002f4a2591bdef
Marc Cluet <marc@ukoncherry.com>
2017-05-25 15:41:23 +0100

    Push version 0.2.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='environment_manager',
-      version='0.2.12',
+      version='0.2.13',
       description="A Client library for Environment Manager",
       url="https://github.com/trainline/python-environment_manager",
       author="Trainline Engineering",
